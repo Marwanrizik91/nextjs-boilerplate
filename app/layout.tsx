b5264a0be2a1e7095c87c77e7e16ba47545f4102
Marwan Rizik <59829281+Marwanrizik91@users.noreply.github.com>
2024-05-25 16:25:40 +0200

    style: init base style

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
+import '@/styles/index.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
